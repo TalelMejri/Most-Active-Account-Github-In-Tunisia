dcc554d3675c764c0ceb1457d1be966e77f58e23
talel <talelmejri8@gmail.com>
2023-05-20 01:35:00 +0100

    add api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,86 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="text-center">
+    <input type="text" v-model="search">
+       <div>
+           <table>
+            <thead>
+               <th>
+                <td>id</td>
+                 <td>name</td>
+               </th>
+              </thead>
+              <tbody>
+                 <tr v-for="user in mysearch" :key="user.id">
+                    <td>{{user.login}}</td>
+                 </tr>
+              </tbody>
+           </table>
+       </div>
+  
   </div>
 </template>
 
 <script>
+import Axios from 'axios';
+import "@/plugins/axios"
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  created(){
+    this.fetchAllUsers();
+  },
+  methods:{
+    async  fetchAllUsers() {
+      let page = 1;
+      const perPage = 100;
+      const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+      const requestDelay = 500;
+  try {
+    while (true) {
+      const response = await axios.get('https://api.github.com/search/users', {
+        params: {
+          //q:"TalelMejri"
+          q: 'location:Tunisia',
+          per_page: perPage,
+          sort:"commits",
+          //sort:"contributions",
+          page,
+        },
+        headers: {
+          Accept: 'application/vnd.github.v3+json',
+          Authorization: 'Bearer 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',
+        },
+      });
+      await delay(requestDelay); 
+      this.AllUser = this.AllUser.concat(response.data.items).slice(0, 200);
+      if (this.AllUser.length < perPage || page==10) {
+        break; 
+      }
+      page++; 
+    }
+    console.log(this.AllUser);
+  } catch (error) {
+    console.error(error);
   }
-}
+},
+},
+  data(){
+    return{
+      AllUser:[],
+      search:""
+    }
+  },
+  computed:{
+         mysearch(){
+            var f=new RegExp(this.search,'i');
+            return this.AllUser.filter(function(el){
+                  return el.login.match(f);
+            })
+         },
+    }
+  }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped lang="scss">
 h3 {
   margin: 40px 0 0;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import './plugins/axios'
+
 import App from './App.vue'
 import router from './router'
 
